1f18163f1c480253b72f00cd1d156b24cef7c26a
pyneninarendra <pyneninarendra@gmail.com>
2021-02-16 08:09:36 +0530

    display raw products

---
+++ src/Product.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+ class Product extends Component {
+    render() {
+        return (
+            <div>
+                <ul className='products'>
+                    {this.props.prod.map((product, index) => 
+                    <li key="index">
+                        <div className="product">
+                            <a href={"#" + product._id}>
+                                <img src={product.image} alt={product.title} />
+                                <p>
+                                    {product.title}
+                                </p>
+                            </a>
+                            <div className="product-price">
+                                <div>{product.price}</div>
+                                <button type="button" className="btn btn-primary">Add To Cart</button>
+                            </div>
+                        </div>
+                    </li>)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Product
